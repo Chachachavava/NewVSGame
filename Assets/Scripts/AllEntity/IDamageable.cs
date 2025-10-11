6a865a1f00e9ea93eac9d14b1b9d6cb1dcadb74d
Chachachavava <sjfehhfknz@gmail.com>
2025-10-12 00:28:42 +0300

    Test

---
+++ Assets/Scripts/AllEntity/IDamageable.cs
@@ -0,0 +1,7 @@
+public interface IDamageable
+{
+    /***
+     * Это нужно унаследовать врагом и играком и типо в нем добавить реализацию этого метода
+     ***/
+    void TakeDamage(float damage);
+}
